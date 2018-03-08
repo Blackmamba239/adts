93fd4125a9becc48424ec87a6644bce707740501
Blackmamba239 <Blackmamba239@users.noreply.github.com>
2018-03-08 17:53:52 -0400

    Did some stuff in ListClient.cpp

--- ListClient.cpp
+++ ListClient.cpp
@@ -11,4 +11,23 @@ int main()
  //Do some stuff with L1, L2, ...
  // ...
 
+ L1.insert(2,1);
+ L1.insert(4,2);
+ L1.insert(6,3);
+ L1.insert(8,4);
+
+ L1.size();
+ L1.empty();
+
+
+ L2.insert(1,1);
+ L2.insert(3,2);
+ L2.insert(7,3);
+ L2.insert(11,4);
+ L2.insert(2,5);
+
+ L2.size();
+ L2.empty();
+
+
 }

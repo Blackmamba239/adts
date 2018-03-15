78f8ef4071373527cceb8857cea8948cd77b6074
Blackmamba239 <Blackmamba239@users.noreply@github.com>
2018-03-15 17:45:13 -0400

    Completed Vector implementation

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,64 @@
 #include "StackV.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int Stack::size()
+{
+
+int i = 0;
+i = data.size();
+
+return i;
+}
+
+
+
+void Stack::push(int node)
+{
+
+int i = 0;
+i = node;
+data.push_back(i);
+
+}
+
+
+void Stack::pop()
+{
+
+data.pop_back();
+
+}
+
+int Stack::top()
+{
+int i(0), a(0);
+i = data.size();
+
+cout<<"top of stack is #" << i << " " <<"Which contains: " << data[i] << endl;
+
+a = data[i];
+
+return a;
+}
+
+
+void Stack::clear()
+{
+int i(0);
+i = data.size();
+
+cout << "Destroying Data....." << endl;
+
+for (int b = 0; b < i; b++)
+{
+
+data.pop_back();
+
+}
+
+cout<< "Done. " << endl;
+}
+
